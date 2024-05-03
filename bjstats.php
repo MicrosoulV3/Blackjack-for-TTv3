99ce92b6ff2a1b8c480a54d2ed7e4241a4bfbb87
MicrosoulV3 <37206645+MicrosoulV3@users.noreply.github.com>
2024-05-03 17:36:19 -0400

    code structure edits

--- bjstats.php
+++ bjstats.php
@@ -1,11 +1,11 @@
 <?php
-require_once("backend/functions.php");
-require_once("backend/config.php");
+require_once "backend/functions.php";
+require_once "backend/config.php";
 
 dbconn(false);
 loggedinonly();
 if ($CURUSER["class"] < "1") {
-    show_error_msg("Sorry...", "You must be a Crazy Seeder or above to play Blackjack.");
+    show_error_msg("Sorry...", "This is stupid and anyone can play.");
 }
 
 $mingames = 2;
@@ -25,23 +25,23 @@ $cachefile = "" . $site_config["cache_dir"] . "/bjstats.txt";
 $cachetime = 60 * 30; // 30 minutes
 // Serve from the cache if it is younger than $cachetime
 if (file_exists($cachefile) && (time() - $cachetime < filemtime($cachefile))) {
-    include($cachefile);
+    include $cachefile;
     print("<p align=center><font class=small>This page last updated " . date('Y-m-d H:i:s', filemtime($cachefile)) . ". </font></p>");
-    
+
     end_frame();
     stdfoot();
-    
+
     exit;
 }
 ob_start(); // start the output buffer
 
 /////////////////////////////////////////////////////////////////////////
 
-function bjtable($res, $frame_caption)
+function bjtable($res, $frameCaption)
 {
-    begin_frame($frame_caption, true);
+    begin_frame($frameCaption, true);
     begin_table();
-?>
+    ?>
     <tr>
     <td class=colhead>Rank</td>
     <td class=colhead align=left>User</td>
@@ -52,23 +52,24 @@ function bjtable($res, $frame_caption)
     <td class=colhead align=right>Win/Loss</td>
     </tr>
     <?php
-    $num = 0;
-    while ($a = mysqli_fetch_assoc($res)) {
+$num = 0;
+    while ($dbqueryreturn = mysqli_fetch_assoc($res)) {
         ++$num;
-        
+
         //Calculate Win %
-        $win_perc = number_format(($a['wins'] / $a['games']) * 100, 1);
-        
+        $winPerc = number_format(($dbqueryreturn['wins'] / $dbqueryreturn['games']) * 100, 1);
+
         // Add a user's +/- statistic
-        $plus_minus = $a['wins'] - $a['losses'];
-        if ($plus_minus >= 0) {
-            $plus_minus = mksize(($a['wins'] - $a['losses'])  * 1024 * 1024 * 1024);
-        } else {
-            $plus_minus = "-";
-            $plus_minus .= mksize(($a['losses'] - $a['wins'])  * 1024 * 1024 * 1024);
+        $plusMinus = $dbqueryreturn['wins'] - $dbqueryreturn['losses'];
+        if ($plusMinus >= 0) {
+            $plusMinus = mksize(($dbqueryreturn['wins'] - $dbqueryreturn['losses']) * 1024 * 1024 * 1024);
         }
-        
-        print("<tr><td>$num</td><td align=left><table border=0 class=main cellspacing=0 cellpadding=0><tr><td class=embedded>" . "<b><a href=userdetails.php?id=" . $a['id'] . ">" . $a['username'] . "</a></b></td>" . "</tr></table></td><td align=right>" . number_format($a['wins'], 0) . "</td>" . "</td><td align=right>" . number_format($a['losses'], 0) . "</td>" . "</td><td align=right>" . number_format($a['games'], 0) . "</td>" . "</td><td align=right>$win_perc</td>" . "</td><td align=right>$plus_minus</td>" . "</tr>\n");
+        {
+            $plusMinus = "-";
+            $plusMinus .= mksize(($dbqueryreturn['losses'] - $dbqueryreturn['wins']) * 1024 * 1024 * 1024);
+        }
+
+        print("<tr><td>$num</td><td align=left><table border=0 class=main cellspacing=0 cellpadding=0><tr><td class=embedded>" . "<b><a href=userdetails.php?id=" . $dbqueryreturn['id'] . ">" . $dbqueryreturn['username'] . "</a></b></td>" . "</tr></table></td><td align=right>" . number_format($dbqueryreturn['wins'], 0) . "</td>" . "</td><td align=right>" . number_format($dbqueryreturn['losses'], 0) . "</td>" . "</td><td align=right>" . number_format($dbqueryreturn['games'], 0) . "</td>" . "</td><td align=right>$winPerc</td>" . "</td><td align=right>$plusMinus</td>" . "</tr>\n");
     }
     end_table();
     end_frame();
@@ -77,7 +78,7 @@ function bjtable($res, $frame_caption)
 // Most Games Played
 $res = SQL_Query_exec("SELECT id, username, bjwins AS wins, bjlosses AS losses, bjwins + bjlosses AS games FROM users WHERE bjwins + bjlosses > $mingames ORDER BY games DESC LIMIT 10");
 
-bjtable($res, "<center>Most Games Played</center>", "Users");
+bjtable($res, "<center>Most Games Played</center>");
 
 print("<br /><br />");
 // /Most Games Played
@@ -85,7 +86,7 @@ print("<br /><br />");
 // Highest Win %
 $res = SQL_Query_exec("SELECT id, username, bjwins AS wins, bjlosses AS losses, bjwins + bjlosses AS games, bjwins / (bjwins + bjlosses) AS winperc FROM users WHERE bjwins + bjlosses > $mingames ORDER BY winperc DESC LIMIT 10");
 
-bjtable($res, "<center>Highest Win Percentage</center>", "Users");
+bjtable($res, "<center>Highest Win Percentage</center>");
 
 print("<br /><br />");
 // /Highest Win %
@@ -93,7 +94,7 @@ print("<br /><br />");
 // Most Credit Won
 $res = SQL_Query_exec("SELECT id, username, bjwins AS wins, bjlosses AS losses, bjwins + bjlosses AS games, bjwins - bjlosses AS winnings FROM users WHERE bjwins + bjlosses > $mingames ORDER BY winnings DESC LIMIT 10");
 
-bjtable($res, "<center>Most Credit Won</center>", "Users");
+bjtable($res, "<center>Most Credit Won</center>");
 
 print("<br /><br />");
 // /Most Credit Won
@@ -101,15 +102,15 @@ print("<br /><br />");
 // Most Credit Lost
 $res = SQL_Query_exec("SELECT id, username, bjwins AS wins, bjlosses AS losses, bjwins + bjlosses AS games, bjlosses - bjwins AS losings FROM users WHERE bjwins + bjlosses > $mingames ORDER BY losings DESC LIMIT 10");
 
-bjtable($res, "<center>Most Credit Lost</center>", "Users");
+bjtable($res, "<center>Most Credit Lost</center>");
 
 // /Most Credit Lost
 
 // CACHE END ////////////////////////////////////////////////////////////
 
-// open the cache file for writing      
+// open the cache file for writing
 $fp = fopen($cachefile, 'w');
-// save the contents of output buffer to the file    
+// save the contents of output buffer to the file
 fwrite($fp, ob_get_contents());
 // close the file
 fclose($fp);
